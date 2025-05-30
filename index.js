62f6778fa0aa9c12c01015b0ee759e60e0e17d85
emauser1 <emauser1@mci.ext>
2025-05-30 10:48:33 +0300

    Initial layout setup: basic structure, header with initial styling, and preliminary dark mode implementation

---
+++ index.js
@@ -0,0 +1,7 @@
+
+//darkMode function
+let bodyDarkMode=document.getElementById('dark-mode');
+bodyDarkMode.addEventListener('click',() => { 
+   document.body.classList.toggle('body-dark-mode');
+  
+ })
